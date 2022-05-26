390d4736e273d2ac6f3334a4cb2246db46ca033e
diyor <diyorbekuralbayev001@gmail.com>
2022-05-26 12:15:42 +0500

    third commit success

--- Task-doc/src/main/java/com/example/taskdoc/config/WebMvcConfig.java
+++ Task-doc/src/main/java/com/example/taskdoc/config/WebMvcConfig.java
@@ -24,76 +24,85 @@ import java.util.concurrent.TimeUnit;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+        registry.addMapping( "/**" ).allowedOrigins( "*" ).allowedMethods( "GET","POST","PUT","DELETE","PATCH" );
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/index.html")
-                .addResourceLocations("classpath:/static/index.html")
-                .setCachePeriod(0);
-
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/")
-                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
-                .resourceChain(false)
-                .addResolver(new PushStateResourceResolver());
+        registry.addResourceHandler( "/**" ).addResourceLocations( "classpath:/static/" )
+                //                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
+                .resourceChain( false ).addResolver( new PushStateResourceResolver() );
     }
 
-
-    private static class PushStateResourceResolver implements ResourceResolver {
-        private Resource index = new ClassPathResource("/static/index.html");
-        private List<String> handledExtensions = Arrays.asList("html", "js", "json", "csv", "css", "png", "svg", "eot", "ttf", "otf", "woff", "appcache", "jpg", "jpeg", "gif", "ico");
-        private List<String> ignoredPaths = Collections.singletonList("api");
+    private class PushStateResourceResolver implements ResourceResolver {
+        private Resource index = new ClassPathResource( "/static/index.html" );
+        private List<String> handledExtensions = Arrays.asList( "html",
+                "js",
+                "json",
+                "csv",
+                "css",
+                "png",
+                "svg",
+                "eot",
+                "ttf",
+                "otf",
+                "woff",
+                "appcache",
+                "jpg",
+                "jpeg",
+                "gif",
+                "ico" );
+        private List<String> ignoredPaths = Arrays.asList( "koinot" );
 
         @Override
-        public Resource resolveResource(HttpServletRequest request, String requestPath, List<? extends Resource> locations, ResourceResolverChain chain) {
-            return resolve(requestPath, locations);
+        public Resource resolveResource(HttpServletRequest request,String requestPath,
+                                        List<? extends Resource> locations,ResourceResolverChain chain) {
+            return resolve( requestPath,locations );
         }
 
         @Override
-        public String resolveUrlPath(String resourcePath, List<? extends Resource> locations, ResourceResolverChain chain) {
-            Resource resolvedResource = resolve(resourcePath, locations);
-            if (resolvedResource == null) {
+        public String resolveUrlPath(String resourcePath,List<? extends Resource> locations,
+                                     ResourceResolverChain chain) {
+            Resource resolvedResource = resolve( resourcePath,locations );
+            if(resolvedResource == null){
                 return null;
             }
-            try {
+            try{
                 return resolvedResource.getURL().toString();
-            } catch (IOException e) {
+            }catch(IOException e){
                 return resolvedResource.getFilename();
             }
         }
 
-        private Resource resolve(String requestPath, List<? extends Resource> locations) {
-            if (isIgnored(requestPath)) {
+        private Resource resolve(String requestPath,List<? extends Resource> locations) {
+            if(isIgnored( requestPath )){
                 return null;
             }
-            if (isHandled(requestPath)) {
+            if(isHandled( requestPath )){
                 return locations.stream()
-                        .map(loc -> createRelative(loc, requestPath))
-                        .filter(resource -> resource != null && resource.exists())
+                        .map( loc -> createRelative( loc,requestPath ) )
+                        .filter( resource -> resource != null && resource.exists() )
                         .findFirst()
-                        .orElseGet(null);
+                        .orElseGet( null );
             }
             return index;
         }
 
-        private Resource createRelative(Resource resource, String relativePath) {
-            try {
-                return resource.createRelative(relativePath);
-            } catch (IOException e) {
+        private Resource createRelative(Resource resource,String relativePath) {
+            try{
+                return resource.createRelative( relativePath );
+            }catch(IOException e){
                 return null;
             }
         }
 
         private boolean isIgnored(String path) {
-            return ignoredPaths.contains(path);
+            return ignoredPaths.contains( path );
         }
 
         private boolean isHandled(String path) {
-            String extension = StringUtils.getFilenameExtension(path);
-            return handledExtensions.stream().anyMatch(ext -> ext.equals(extension));
+            String extension = StringUtils.getFilenameExtension( path );
+            return handledExtensions.stream().anyMatch( ext -> ext.equals( extension ) );
         }
     }
-
 }
